549d1b2bd109c1df2d1a5b229789c3a211585a98
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2023-12-03 11:07:31 +0100

    parser

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -1,1 +1,11 @@
-fn main() {}
+fn main() {
+    let raw_input = std::fs::read_to_string("input.txt").unwrap();
+    let input = parse_input(&raw_input);
+}
+
+fn parse_input(input: &str) -> Vec<Vec<&str>> {
+    input
+        .split("\n")
+        .map(|line| line.split("").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>()
+}
