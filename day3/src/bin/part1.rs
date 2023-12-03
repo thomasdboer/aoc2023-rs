e7a74243c66c6505363757736a044f07d623a40c
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2023-12-03 16:48:19 +0100

    some output

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 struct DigitSequence {
     value: usize,
     x_start: usize,
@@ -5,13 +7,10 @@ struct DigitSequence {
     y: usize,
 }
 
-const NOT_PART_NUMBER_REGEX: &str = r"[^.0-9]";
-
 fn main() {
     let raw_input = std::fs::read_to_string("input.txt").unwrap();
     let grid = parse_input(&raw_input);
     let part_numbers = find_part_numbers(&grid);
-    part_numbers.iter().for_each(|part_number| println!("{}", part_number.value));
     let answer: usize = part_numbers
         .iter()
         .map(|part_number| part_number.value)
@@ -31,6 +30,7 @@ fn find_part_numbers(grid: &Vec<Vec<&str>>) -> Vec<DigitSequence> {
     for (y, line) in grid.iter().enumerate() {
         let digit_sequences: Vec<DigitSequence> = get_digits_from_line(line.clone(), y);
         for digit_sequence in digit_sequences {
+            println!("{}", digit_sequence.value);
             if is_part_number(&digit_sequence, grid) {
                 part_numbers.push(digit_sequence);
             }
@@ -40,10 +40,11 @@ fn find_part_numbers(grid: &Vec<Vec<&str>>) -> Vec<DigitSequence> {
 }
 
 fn get_digits_from_line(line: Vec<&str>, y: usize) -> Vec<DigitSequence> {
+    let number_regex: Regex = Regex::new(r"\d").unwrap();
     let mut digit_sequences = Vec::new();
     let (mut x_start, mut x_end) = (0, 0);
     for (x, character) in line.iter().enumerate() {
-        if !character.matches(NOT_PART_NUMBER_REGEX).collect::<Vec<&str>>().is_empty() {
+        if !number_regex.is_match(character) {
             if x_start != x_end {
                 digit_sequences.push(DigitSequence {
                     value: line[x_start..x_end].join("").parse::<usize>().unwrap_or(0),
@@ -66,16 +67,22 @@ fn get_digits_from_line(line: Vec<&str>, y: usize) -> Vec<DigitSequence> {
 }
 
 fn is_part_number(sequence: &DigitSequence, grid: &Vec<Vec<&str>>) -> bool {
-    let mut is_part_number = true;
-    for y in sequence.y - 1..=sequence.y + 1 {
-        for x in sequence.x_start..sequence.x_end {
-            if y < grid.len() && x < grid[y].len() {
-                if !grid[y][x].matches(NOT_PART_NUMBER_REGEX).collect::<Vec<&str>>().is_empty() {
-                    is_part_number = false;
-                }
-            }
+    let part_number_regex: Regex = Regex::new(r"[^\d.]").unwrap();
+    let mut is_part_number = false;
+    let (x_start, x_end, y) = (sequence.x_start, sequence.x_end, sequence.y);
+    let mut adjacent_characters = Vec::new();
+    if y > 0 {
+        adjacent_characters.push(grid[y - 1][x_start - 1..x_end + 1].join(""));
+    }
+    adjacent_characters.push(grid[y][x_start - 1..x_end + 1].join(""));
+    if y < grid.len() - 1 {
+        adjacent_characters.push(grid[y + 1][x_start - 1..x_end - 1].join(""));
+    }
+    for adjacent_character in adjacent_characters {
+        if !part_number_regex.is_match(&adjacent_character) {
+            is_part_number = true;
         }
     }
-    print!("{} ", is_part_number);
+    println!("{}", is_part_number.to_string());
     is_part_number
 }
