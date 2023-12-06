61848e1e45a9f426522029deba1beb1a694d8846
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2023-12-06 08:43:49 +0100

    idk whats wrong

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -9,17 +9,18 @@ struct DigitSequence {
 
 fn main() {
     let raw_input = std::fs::read_to_string("input.txt").unwrap();
-    let grid = parse_input(&raw_input);
-    let part_numbers = find_part_numbers(&grid);
+    let grid: Vec<Vec<&str>> = parse_input(&raw_input);
+    let part_numbers: Vec<DigitSequence> = find_part_numbers(&grid);
     let answer: usize = part_numbers
         .iter()
-        .map(|part_number| part_number.value)
+        .map(|part_number: &DigitSequence| part_number.value)
         .sum();
     println!("Answer: {}", answer);
 }
 
 fn parse_input(input: &str) -> Vec<Vec<&str>> {
     input
+        .trim_end()
         .split("\n")
         .map(|line| line.split("").collect::<Vec<&str>>())
         .collect::<Vec<Vec<&str>>>()
@@ -30,7 +31,6 @@ fn find_part_numbers(grid: &Vec<Vec<&str>>) -> Vec<DigitSequence> {
     for (y, line) in grid.iter().enumerate() {
         let digit_sequences: Vec<DigitSequence> = get_digits_from_line(line.clone(), y);
         for digit_sequence in digit_sequences {
-            println!("{}", digit_sequence.value);
             if is_part_number(&digit_sequence, grid) {
                 part_numbers.push(digit_sequence);
             }
@@ -40,49 +40,46 @@ fn find_part_numbers(grid: &Vec<Vec<&str>>) -> Vec<DigitSequence> {
 }
 
 fn get_digits_from_line(line: Vec<&str>, y: usize) -> Vec<DigitSequence> {
-    let number_regex: Regex = Regex::new(r"\d").unwrap();
+    let number_regex = Regex::new(r"[0-9]").unwrap();
     let mut digit_sequences = Vec::new();
     let (mut x_start, mut x_end) = (0, 0);
     for (x, character) in line.iter().enumerate() {
         if !number_regex.is_match(character) {
-            if x_start != x_end {
+            if x_end > x_start {
                 digit_sequences.push(DigitSequence {
                     value: line[x_start..x_end].join("").parse::<usize>().unwrap_or(0),
                     x_start,
                     x_end,
                     y,
                 });
-                digit_sequences
-                    .iter()
-                    .for_each(|digit_sequence| { println!("{}", digit_sequence.value) });
-
-                x_start = x + 1;
-                x_end = x + 1;
-            } else {
-                x_end += 1;
             }
+            x_start = x + 1;
         }
+        x_end += 1;
     }
     digit_sequences
 }
 
 fn is_part_number(sequence: &DigitSequence, grid: &Vec<Vec<&str>>) -> bool {
-    let part_number_regex: Regex = Regex::new(r"[^\d.]").unwrap();
-    let mut is_part_number = false;
-    let (x_start, x_end, y) = (sequence.x_start, sequence.x_end, sequence.y);
-    let mut adjacent_characters = Vec::new();
-    if y > 0 {
-        adjacent_characters.push(grid[y - 1][x_start - 1..x_end + 1].join(""));
-    }
-    adjacent_characters.push(grid[y][x_start - 1..x_end + 1].join(""));
-    if y < grid.len() - 1 {
-        adjacent_characters.push(grid[y + 1][x_start - 1..x_end - 1].join(""));
-    }
-    for adjacent_character in adjacent_characters {
-        if !part_number_regex.is_match(&adjacent_character) {
-            is_part_number = true;
-        }
-    }
-    println!("{}", is_part_number.to_string());
-    is_part_number
+    let not_part_number_regex = Regex::new(r"[0-9.]").unwrap();
+    let surrounding_characters: Vec<&str> = get_surrounding_characters(sequence, grid);
+    !surrounding_characters.iter().all(|num| not_part_number_regex.is_match(num))
+}
+
+fn get_surrounding_characters<'a>(
+    sequence: &DigitSequence,
+    grid: &'a Vec<Vec<&str>>
+) -> Vec<&'a str> {
+    let (x_max, y_max) = (grid[0].len() - 1, grid.len() - 1);
+    let (x_min, y_min) = (0, 0);
+    let (y1, y2, x1, x2) = (
+        if sequence.y > y_min { sequence.y - 1 } else { sequence.y },
+        if sequence.y < y_max { sequence.y + 1 } else { sequence.y },
+        if sequence.x_start > x_min { sequence.x_start - 1 } else { sequence.x_start },
+        if sequence.x_end < x_max { sequence.x_end + 1 } else { sequence.x_end },
+    );
+    grid[y1..=y2]
+        .iter()
+        .flat_map(|line| { line[x1..=x2].to_vec() })
+        .collect::<Vec<&'a str>>()
 }
