aea1632d4f0a3bdfe6359292a027cea0a437c3a9
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2023-12-09 23:00:35 +0100

    not quite

--- day5/src/bin/part1.rs
+++ day5/src/bin/part1.rs
@@ -3,69 +3,80 @@ struct ValueMap {
     destination_range_start: usize,
     range_length: usize,
 }
-fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
+
+struct Seed {
+    value: usize,
 }
 
-// example input
-// seeds: 79 14 55 13
+struct Puzzle {
+    value_maps: Vec<Vec<ValueMap>>,
+    seeds: Vec<Seed>,
+}
 
-// seed-to-soil map:
-// 50 98 2
-// 52 50 48
-
-// soil-to-fertilizer map:
-// 0 15 37
-// 37 52 2
-// 39 0 15
-
-// fertilizer-to-water map:
-// 49 53 8
-// 0 11 42
-// 42 0 7
-// 57 7 4
-
-// water-to-light map:
-// 88 18 7
-// 18 25 70
-
-// light-to-temperature map:
-// 45 77 23
-// 81 45 19
-// 68 64 13
-
-// temperature-to-humidity map:
-// 0 69 1
-// 1 0 69
-
-// humidity-to-location map:
-// 60 56 37
-// 56 93 4
-fn parse_input(input: &str) -> Vec<Vec<ValueMap>> {
-    let mut result = Vec::new();
-    let parts = input.split(":").collect::<Vec<&str>>();
-    let parts_iter = parts.iter();
-    let seeds = &parts_iter
-        .next()
-        .unwrap()
-        .split(" ")
-        .map(|seed| seed.parse::<usize>().unwrap())
+fn main() {
+    let input: String = std::fs::read_to_string("input.txt").unwrap();
+    let puzzle: Puzzle = parse_input(&input);
+    let location_numbers = puzzle.seeds
+        .iter()
+        .map(|seed| find_location(seed, 0, &puzzle))
         .collect::<Vec<usize>>();
-    // For every line in the seed to soil map, construct a ValueMap of each of the three values in the line.
-    let seed_to_soil_map = &parts_iter
-        .next()
-        .unwrap()
-        .split("\n")
-        .map(|line| {
-            let x = line.split(" ").next();
-            println!("X: {:?}", x);
-            return ValueMap {
-                source_range_start: 0,
-                destination_range_start: 0,
-                range_length: 0,
-            };
+    println!("Answer: {:?}", location_numbers.iter().min());
+}
+
+fn find_location(seed: &Seed, round: usize, puzzle: &Puzzle) -> usize {
+    let mut new_seed = seed.value;
+    for map in &puzzle.value_maps[round] {
+        if
+            new_seed >= map.source_range_start &&
+            new_seed < map.source_range_start + map.range_length
+        {
+            new_seed = map.destination_range_start + (new_seed - map.source_range_start);
+        }
+    }
+    if round < puzzle.value_maps.len() - 1 {
+        new_seed = find_location(&(Seed { value: new_seed }), round + 1, puzzle);
+    }
+    new_seed
+}
+
+fn parse_input(input: &str) -> Puzzle {
+    let sections: Vec<&str> = input.split(":").collect::<Vec<&str>>();
+
+    // the second section after the colon contains the seeds
+    let seeds: Vec<Seed> = sections[1]
+        .split(" ")
+        .filter(|value| { value.chars().all(|c| c.is_numeric()) && !value.is_empty() })
+        .map(|value| Seed { value: value.parse::<usize>().unwrap() })
+        .collect::<Vec<Seed>>();
+
+    let value_maps: Vec<Vec<ValueMap>> = sections
+        .iter()
+        .skip(1)
+        .map(|section| {
+            section
+                .split("\n")
+                .filter(|line| !line.contains("map") && !line.is_empty())
+                .map(|line| value_map_from_line(&line))
+                .collect::<Vec<ValueMap>>()
         })
-        .collect::<Vec<ValueMap>>();
-    result.push(seed_to_soil_map);
-    result
+        .collect::<Vec<Vec<ValueMap>>>();
+
+    Puzzle {
+        value_maps,
+        seeds,
+    }
+}
+
+fn value_map_from_line(line: &str) -> ValueMap {
+    let values = line
+        .split(" ")
+        .filter(|value| { value.chars().all(|c| c.is_numeric()) && !value.is_empty() })
+        .map(|value| value.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    ValueMap {
+        source_range_start: values[0],
+        destination_range_start: values[1],
+        range_length: values[2],
+    }
 }
