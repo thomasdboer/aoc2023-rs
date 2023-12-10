c040b8d1f9a27fde627bb9b334c9c5085ee9d84c
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2023-12-10 15:15:00 +0100

    panic at the rustco

--- day5/src/bin/part1.rs
+++ day5/src/bin/part1.rs
@@ -1,11 +1,11 @@
 struct ValueMap {
-    source_range_start: usize,
-    destination_range_start: usize,
-    range_length: usize,
+    source_range_start: u32,
+    destination_range_start: u32,
+    range_length: u32,
 }
 
 struct Seed {
-    value: usize,
+    value: u32,
 }
 
 struct Puzzle {
@@ -15,62 +15,73 @@ struct Puzzle {
 
 fn main() {
     let input: String = std::fs::read_to_string("input.txt").unwrap();
-    let puzzle: Puzzle = parse_input(&input);
+    let sections = input.split(":").collect::<Vec<&str>>();
+    let puzzle: Puzzle = parse_input(sections);
     let location_numbers = puzzle.seeds
         .iter()
-        .map(|seed| find_location(seed, 0, &puzzle))
-        .collect::<Vec<usize>>();
-    println!("Answer: {:?}", location_numbers.iter().min());
+        .map(|seed| {
+            println!("Seed: {}", seed.value);
+            find_location(seed, 0, &puzzle)
+        })
+        .collect::<Vec<u32>>();
+    println!("Answer: {:?}", location_numbers.iter().min().unwrap());
 }
 
-fn find_location(seed: &Seed, round: usize, puzzle: &Puzzle) -> usize {
+fn find_location(seed: &Seed, round: usize, puzzle: &Puzzle) -> u32 {
     let mut new_seed = seed.value;
+    let old_seed = new_seed.clone();
     for map in &puzzle.value_maps[round] {
         if
-            new_seed >= map.source_range_start &&
-            new_seed < map.source_range_start + map.range_length
+            old_seed >= map.source_range_start &&
+            old_seed < map.source_range_start + map.range_length
         {
-            new_seed = map.destination_range_start + (new_seed - map.source_range_start);
+            new_seed = map.destination_range_start + (old_seed - map.source_range_start);
+            break;
         }
     }
+
     if round < puzzle.value_maps.len() - 1 {
         new_seed = find_location(&(Seed { value: new_seed }), round + 1, puzzle);
     }
     new_seed
 }
 
-fn parse_input(input: &str) -> Puzzle {
-    let sections: Vec<&str> = input.split(":").collect::<Vec<&str>>();
+fn parse_input(sections: Vec<&str>) -> Puzzle {
     Puzzle {
         seeds: sections[1]
-            .split(" ")
-            .filter(|value| { value.chars().all(|c| c.is_numeric()) && !value.is_empty() })
-            .map(|value| Seed { value: value.parse::<usize>().unwrap() })
+            .split("\n")
+            .take(1)
+            .flat_map(|line| line.split(" "))
+            .filter(|value| value.chars().all(|c| c.is_numeric()) && !value.is_empty())
+            .map(|value| Seed { value: value.parse::<u32>().unwrap() })
             .collect::<Vec<Seed>>(),
+
         value_maps: sections
             .iter()
             .skip(1)
-            .map(|section| {
-                section
-                    .split("\n")
-                    .filter(|line| !line.contains("map") && !line.is_empty())
-                    .map(|line| value_map_from_line(&line))
-                    .collect::<Vec<ValueMap>>()
-            })
+            .map(|section| { value_maps_from_section(section) })
             .collect::<Vec<Vec<ValueMap>>>(),
     }
+}
+
+fn value_maps_from_section(section: &str) -> Vec<ValueMap> {
+    section
+        .split("\n")
+        .filter(|line| !line.contains("map") && !line.is_empty())
+        .map(|line| value_map_from_line(&line))
+        .collect::<Vec<ValueMap>>()
 }
 
 fn value_map_from_line(line: &str) -> ValueMap {
     let values = line
         .split(" ")
         .filter(|value| { value.chars().all(|c| c.is_numeric()) && !value.is_empty() })
-        .map(|value| value.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>();
+        .map(|value| value.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
 
     ValueMap {
-        source_range_start: values[0],
-        destination_range_start: values[1],
+        destination_range_start: values[0],
+        source_range_start: values[1],
         range_length: values[2],
     }
 }
